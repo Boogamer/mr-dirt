8eefe4bf02ab9480082e4324a1ed34fbdda55252
Ugo Parlange <uparlange@gmail.com>
2020-08-22 23:21:10 +0200

    site optimisation

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
 
-fs.copyFileSync("./node_modules/bootstrap-icons/bootstrap-icons.svg", "./site/static/assets/bootstrap-icons.svg");
+fs.copyFileSync(__dirname + "/node_modules/bootstrap-icons/bootstrap-icons.svg", __dirname + "/site/static/assets/bootstrap-icons.svg");
